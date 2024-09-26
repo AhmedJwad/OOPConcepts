b46acaff038254a9c1d02a54de553dd60c6bbb75
Ahmednet380 <Ahmednet380@gmail.com>
2024-09-26 11:39:51 +0300

    Fixed sorting algorithm

---
+++ OOPConcepts.Logic/MyArray.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPConcepts.Logic
+{
+   public class MyArray
+    {
+        private int _top;
+        private int[] _array;
+
+        public MyArray(int n)
+        {
+            N = n;
+            _array = new int[N];
+            _top = 0;
+        }
+
+        public int N { get; }
+        /// <summary>
+        /// Indicates if the array is full
+        /// </summary>
+        public bool IsFull => _top == N;
+
+        /// <summary>
+        /// Indicates if the array is empty
+        /// </summary>
+        public bool IsEmpty => _top == 0;
+
+        public void Add(int number)
+        {
+           
+            if(IsFull)
+            {
+                throw new Exception("The array is full.");
+            }
+            _array[_top] = number;
+            _top++;
+        }
+        public override string ToString()
+        {
+            if (IsEmpty)
+            {
+                return "Array is empty";
+            }
+            string output = string.Empty;
+            int count = 0;
+            for (int i = 0; i < _top; i++)
+            {
+                output += $"{_array[i]}\t";
+                count++;
+                if(count > 9)
+                {
+                    count = 0;
+                    output += "\n";
+                }
+            }
+            return output;
+        }
+
+        public void Fill (int minimuim, int maximuim)
+        {
+            var random= new Random();
+            for (int i = 0; i <N; i++)
+            {
+                _array[i] = random.Next(minimuim, maximuim);
+            }
+            _top = N;
+        }
+        public void Sort()
+        {
+            Sort(true);
+        }
+        private void Sort(bool descending)
+        {
+            for (int i = 0;i < _top-1;i++)
+            {
+                for (int j = i+1; j < _top; j++)
+                {
+                    if(descending)
+                    {
+                        if (_array[i] < _array[j])
+                        {
+                            Change(ref _array[i],ref _array[j]);
+                        }
+                    }
+                    else
+                    {
+                        if(_array[i] > _array[j])
+                        {
+                            Change(ref _array[i], ref _array[j]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void Change(ref int a ,ref int b)
+        {
+            //a=10 ,b=5
+            
+            a = a + b; //a=15
+            b = a - b; //b=10
+            a = a - b; //a=5
+        }
+    }
+}
--- OOPConcepts/Program.cs
+++ OOPConcepts/Program.cs
@@ -13,108 +13,119 @@ using OOPConcepts.Logic;
 
 //    Console.WriteLine($"Error: {ex.Message}");
 //}
-try
-{
-	var emplyoeee1 = new SalaryEmployee
-	{
-		Id = 1010,
-		BornDate = new(1980, 07, 14),
-		HireDate = new(2021, 09, 1),
-		FirstName = "john",
-		LastName = "brait",
-		isActive = true,
-		Salary = 1050000,
+//try
+//{
+//	var emplyoeee1 = new SalaryEmployee
+//	{
+//		Id = 1010,
+//		BornDate = new(1980, 07, 14),
+//		HireDate = new(2021, 09, 1),
+//		FirstName = "john",
+//		LastName = "brait",
+//		isActive = true,
+//		Salary = 1050000,
 
-	};
-    var emplyoeee2 = new HourlyEmployee
-    { 
-        Id = 1020,
-        BornDate = new(1983, 07, 14),
-        HireDate = new(2021, 09, 1),
-        FirstName = "john",
-        LastName = "brait",
-        isActive = true,
-        WorkingHours = 123.5f,
-        HourValue=120000,
+//	};
+//    var emplyoeee2 = new HourlyEmployee
+//    { 
+//        Id = 1020,
+//        BornDate = new(1983, 07, 14),
+//        HireDate = new(2021, 09, 1),
+//        FirstName = "john",
+//        LastName = "brait",
+//        isActive = true,
+//        WorkingHours = 123.5f,
+//        HourValue=120000,
 
-    };
+//    };
 
-var emplyoeee3 = new CommissionEmployee
-{
-    Id = 1020,
-    BornDate = new(1983, 07, 14),
-    HireDate = new(2021, 09, 1),
-    FirstName = "john",
-    LastName = "brait",
-    isActive = true,
-    CommissionPercentage = 123.5f,
-    Sales = 120000,
+//var emplyoeee3 = new CommissionEmployee
+//{
+//    Id = 1020,
+//    BornDate = new(1983, 07, 14),
+//    HireDate = new(2021, 09, 1),
+//    FirstName = "john",
+//    LastName = "brait",
+//    isActive = true,
+//    CommissionPercentage = 123.5f,
+//    Sales = 120000,
 
-};
-    var emplyoeee4 = new BaseCommissionEmployee
-    {
-        Id = 1020,
-        BornDate = new(1983, 07, 14),
-        HireDate = new(2021, 09, 1),
-        FirstName = "john",
-        LastName = "brait",
-        isActive = true,
-        CommissionPercentage = 123.5f,
-        Sales = 120000,
-        Salary=600000,
+//};
+//    var emplyoeee4 = new BaseCommissionEmployee
+//    {
+//        Id = 1020,
+//        BornDate = new(1983, 07, 14),
+//        HireDate = new(2021, 09, 1),
+//        FirstName = "john",
+//        LastName = "brait",
+//        isActive = true,
+//        CommissionPercentage = 123.5f,
+//        Sales = 120000,
+//        Salary=600000,
 
-    };
+//    };
 
-    Employee[] employees = new Employee[] { emplyoeee1, emplyoeee2, emplyoeee3, emplyoeee4 };
-    decimal payRoll = 0;
-    foreach (var item in employees)
-    {
-        Console.WriteLine(item);
-        payRoll += item.GetValueToPay();
-    }
-    Console.WriteLine("$.....================");
-    Console.WriteLine($"$Total Payment  ..==={payRoll}");
+//    Employee[] employees = new Employee[] { emplyoeee1, emplyoeee2, emplyoeee3, emplyoeee4 };
+//    decimal payRoll = 0;
+//    foreach (var item in employees)
+//    {
+//        Console.WriteLine(item);
+//        payRoll += item.GetValueToPay();
+//    }
+//    Console.WriteLine("$.....================");
+//    Console.WriteLine($"$Total Payment  ..==={payRoll}");
 
-    Invoice invoice1 = new Invoice
-    {
-        Id = 1010,
-        Description="test1",
-        Quantity=10,
-        Price=20,
-    };
-    Invoice invoice2 = new Invoice
-    {
-        Id = 1020,
-        Description = "test2",
-        Quantity = 10,
-        Price = 20,
-    };
-    Invoice invoice3 = new Invoice
-    {
-        Id = 1030,
-        Description = "test3",
-        Quantity = 10,
-        Price = 20,
-    };
-    Invoice invoice4 = new Invoice
-    {
-        Id = 1040,
-        Description = "test4",
-        Quantity = 10,
-        Price = 20,
-    };
-    Invoice[] invoices= new Invoice[] { invoice1, invoice2, invoice3, invoice4 };
-    decimal TotalInvoice = 0;
-    foreach (var item in invoices) 
-    { 
-        Console.WriteLine(item);
-        TotalInvoice += item.GetValuetoPay();
-    }
-    Console.WriteLine("$.....================");
-    Console.WriteLine($"$TotalInvoice  ..==={TotalInvoice}");
-}
-catch (Exception ex)
-{
+//    Invoice invoice1 = new Invoice
+//    {
+//        Id = 1010,
+//        Description="test1",
+//        Quantity=10,
+//        Price=20,
+//    };
+//    Invoice invoice2 = new Invoice
+//    {
+//        Id = 1020,
+//        Description = "test2",
+//        Quantity = 10,
+//        Price = 20,
+//    };
+//    Invoice invoice3 = new Invoice
+//    {
+//        Id = 1030,
+//        Description = "test3",
+//        Quantity = 10,
+//        Price = 20,
+//    };
+//    Invoice invoice4 = new Invoice
+//    {
+//        Id = 1040,
+//        Description = "test4",
+//        Quantity = 10,
+//        Price = 20,
+//    };
+//    Invoice[] invoices= new Invoice[] { invoice1, invoice2, invoice3, invoice4 };
+//    decimal TotalInvoice = 0;
+//    foreach (var item in invoices) 
+//    { 
+//        Console.WriteLine(item);
+//        TotalInvoice += item.GetValuetoPay();
+//    }
+//    Console.WriteLine("$.....================");
+//    Console.WriteLine($"$TotalInvoice  ..==={TotalInvoice}");
+//}
+//catch (Exception ex)
+//{
 
-    Console.WriteLine($"Error: {ex.Message}");
-}
+//    Console.WriteLine($"Error: {ex.Message}");
+//}
+MyArray array = new (10);
+//unsorted array
+array.Fill(1, 10);
+Console.WriteLine("unsorted array");
+Console.WriteLine(array);
+
+//sorted Array 
+
+array.Sort();
+Console.WriteLine("Sorted array");
+Console.WriteLine(array);
